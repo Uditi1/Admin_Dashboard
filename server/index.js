7f096d9fe8678f681ab2ca3833f4b372997a6129
Uditi1 <udititiwari@gmail.com>
2023-03-28 22:45:26 +0530

    first commit 💀💀

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/mangagement.js'
+import salesRoutes from './routes/sales.js'
+
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use(cors())
+
+
+app.use("/client", clientRoutes)
+app.use("/general", generalRoutes)
+app.use("/management", managementRoutes)
+app.use("/sales", salesRoutes)
