1b15f8f60fe7a370bf7711c6cd35e11574df4894
Uditi1 <udititiwari@gmail.com>
2023-03-30 21:04:43 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import clientRoutes from './routes/client.js'
 import generalRoutes from './routes/general.js'
-import managementRoutes from './routes/mangagement.js'
+import managementRoutes from './routes/management.js'
 import salesRoutes from './routes/sales.js'
 
 dotenv.config();
@@ -17,7 +17,7 @@ app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
 app.use(morgan("common"))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 
 
@@ -25,3 +25,11 @@ app.use("/client", clientRoutes)
 app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
+
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server PORT: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
